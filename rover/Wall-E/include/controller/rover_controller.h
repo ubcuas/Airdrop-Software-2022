e2c682f089b1208e7adf5ef711074db1ed8100fe
mackenziemar3@gmail.com <mackenziemar3@gmail.com>
2020-11-21 15:41:38 -0700

    controller code

--- rover/Wall-E/include/controller/rover_controller.h
+++ rover/Wall-E/include/controller/rover_controller.h
@@ -3,23 +3,38 @@
 
 namespace controller
 {
-    class RoverController{
-        private:
+    class RoverController
+    {
+       private:
+       public:
+        RoverController();
 
-        public:
-            RoverController();
-            static std::pair<double, double> MotorController(int throttle, int turn_angle);
-            static std::pair<double, double> RCController(int throttle_value, int yaw_value);
+        /**
+         * @brief
+         *
+         * @param throttle 0-100 percent
+         * @param turn_angle -180 to 180 degree, clockwise positive
+         * @return std::pair<double, double> left motor speed, right motor speed
+         */
+        static std::pair<double, double> MotorController(int throttle, int turn_angle);
 
-            /**
-             * @brief Landing detection algorithm. Takes input from 
-             * 
-             * @todo complete function signature. 
-             * 
-             */
-            void LandingDetectionUpdate();
-            
-            bool GetLandingStatus();
+        /**
+         * @brief
+         *
+         * @param throttle_value 1000-2000
+         * @param yaw_value 1000-2000
+         * @return std::pair<double, double> speed, turn angle
+         */
+        static std::pair<double, double> RCController(int throttle_value, int yaw_value);
 
+        /**
+         * @brief Landing detection algorithm. Takes input from
+         *
+         * @todo complete function signature.
+         *
+         */
+        void LandingDetectionUpdate();
+
+        bool GetLandingStatus();
     };
-} // namespace controller
+}  // namespace controller
