575003f1b37385235067b9a71505b577cb425569
Rainerino <albertyanyy@gmail.com>
2020-11-28 12:56:52 -0800

    add test backward

--- rover/Wall-E/test/test_main.cpp
+++ rover/Wall-E/test/test_main.cpp
@@ -50,7 +50,7 @@ void setup()
     RUN_TEST(test_controller_driver_left);
     RUN_TEST(test_controller_driver_right);
     RUN_TEST(test_controller_driver_out_of_bound);
-
+    RUN_TEST(test_controller_driver_backward);
     // example blink
     RUN_TEST(test_led_builtin_pin_number);
 
